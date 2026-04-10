3841968d9371b99dbaa348c8b4bf9e6066b7a2ed
whghdus <whghdus04@gmail.com>
2026-04-11 03:16:45 +0900

    "미션 5주차"

--- umc10th/src/main/java/com/example/umc10th/domain/member/dto/MemberReqDTO.java
+++ umc10th/src/main/java/com/example/umc10th/domain/member/dto/MemberReqDTO.java
@@ -1,4 +1,16 @@
 package com.example.umc10th.domain.member.dto;
 
+import lombok.Getter;
+
 public class MemberReqDTO {
+    @Getter
+    public static class RequestBody {
+        private String stringTest;
+        private Long longTest;
+    }
+
+    //마이페이지
+    public record GetInfo(
+            Long id
+    ){}
 }
